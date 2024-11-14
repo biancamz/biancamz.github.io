bc679e46d516f620cb6abf66c64f883ecf58d6d8
biancamz <bime1968@colorado.edu>
2024-11-14 15:10:06 -0700

    wa12 updates

--- wa/WA12/wa12.js
+++ wa/WA12/wa12.js
@@ -16,20 +16,20 @@ fetch(`https://corsproxy.io/?https://xkcd.com/${comicNumber}/info.0.json`)
         console.log(myData);
         document.getElementById('comicTitle').innerHTML = myData.title;
         document.getElementById('comicImg').src= myData.img;
-        document.getElementById('comicAltText').setAttribute("alt".myData.alt);
+        document.getElementById('comicAltText').setAttribute("alt", myData.alt);
 
         let m = myData.month;
         let d = myData.day;
         let y = myData.year;
 
-        document.getElementById('comicDate').innerHTML = (m + '/' + d + '/' + y)
+        document.getElementById('comicDate').innerHTML = (m +'/'+ d +'/'+ y);
 
     })
     .catch(error => {console.log(error)})
 }
 fetchData();
 
-document.getElementById("generate").addEventListener("click", e=> {fetchData();})
+document.getElementById('generate').addEventListener("click", e=> {fetchData();})
 
 console.log(myData);
 
